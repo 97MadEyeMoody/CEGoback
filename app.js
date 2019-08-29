a7364b80b8a5595497f35478366c7555cb0052df
abhinand <abhinandrkvatakara@gmail.com>
2019-08-29 12:38:01 +0530

    sencond commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
  
+let input ;
 fs.readFile('/home/nithin/sandbox/chargebee-migration/input.json','utf8', (err, contents) => {
     if (err) {
         console.error(err)
         return
       }
-    console.log(contents);
+      input = contents;
+    
 });
 
